18f0e05922589f8c1c6c3360f249537d7d668df3
INDRIAN <indrian16@outlook.co.id>
2020-07-05 01:41:38 +0800

    add date, time, weather condition

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
 // Bind View
-
 const inputName = document.getElementById("name_city")
-
 const cityName = document.getElementById("city")
 const wMain = document.getElementById("weather-main")
 const wDesc = document.getElementById("weather-desc")
 const mainTemp = document.getElementById("weather-temp")
+const dateView = document.getElementById("date")
+const timeView = document.getElementById("time")
+
+// Start Background View
+const startBG = document.getElementById("start")
+// Default Background
+updateBackground("01d")
+
+// Get Current Time
+getCurrentTime()
+
+// Update DateTime every 1 sec
+setInterval(() => {
+    getCurrentTime()
+}, 1000); // 1000ms
 
 // Get Default Weather
 getWeather("samarinda")
 
-// Update Weather
+// Search Button Listener
 function btnSearch() {
     getWeather(inputName.value)
 }
--- js/view.js
+++ js/view.js
@@ -17,4 +17,74 @@ function updateView(data) {
     const coord = data.coord
     console.log(`Coord: ${coord.lat}, ${coord.lon}`)
     updateMap(coord.lat, coord.lon)
+
+    // Condition
+    const condition = weather.icon
+    updateBackground(condition)
+}
+
+function getCurrentTime() {
+
+    // List of Month Name
+    const monthNames = [
+        "January", 
+        "February", 
+        "March", 
+        "April", 
+        "May", 
+        "June",
+        "July", 
+        "August", 
+        "September", 
+        "October", 
+        "November", 
+        "December"
+    ];
+
+    // Init Date
+    const date = new Date()
+    
+    // Create Date String
+    const dateStr = date.getDate()
+    const monthStr = monthNames[date.getMonth()]
+    const yearStr = date.getFullYear()
+
+    // Create Time String
+    const hourStr = date.getUTCHours()
+    const minuteStr = date.getMinutes()
+    const secStr = date.getSeconds()
+
+
+    // Update Date Text
+    dateView.innerText = dateStr + " " + monthStr + " " + yearStr
+
+    // Update Time Text
+    timeView.innerText = hourStr + ":" + minuteStr + ":" + secStr
+}
+
+function updateBackground(condition) {
+
+    const conditionMap = {
+        "01d" : 'url(images/condition/clear_sky_d.jpg)',
+        "01n" : 'url(images/condition/clear_sky_n.jpg)',
+        "02d" : 'url(images/condition/few_cloud_d.jpg)',
+        "02n" : 'url(images/condition/few_cloud_n.jpg)',
+        "03d" : 'url(images/condition/scattered_d.jpg)',
+        "03n" : 'url(images/condition/scattered_n.jpg)',
+        "04d" : 'url(images/condition/scattered_d.jpg)',
+        "04n" : 'url(images/condition/scattered_n.jpg)',
+        "09d" : 'url(images/condition/rain_d.jpg)',
+        "09n" : 'url(images/condition/rain_n.jpg)',
+        "10d" : 'url(images/condition/rain_d.jpg)',
+        "10n" : 'url(images/condition/rain_n.jpg)',
+        "11d" : 'url(images/condition/thunderstorm_d.jpg)',
+        "11n" : 'url(images/condition/thunderstorm_n.jpg)',
+        "13d" : 'url(images/condition/snow_d.jpg)',
+        "13n" : 'url(images/condition/snow_n.jpg)',
+        "50d" : 'url(images/condition/fog_d.jpg)',
+        "50n" : 'url(images/condition/fog_n.jpg)'
+    }
+    
+    // Change BG
+    startBG.style.background = conditionMap[condition]
 }
